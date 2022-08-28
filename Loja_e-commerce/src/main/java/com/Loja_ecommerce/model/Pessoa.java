202c9123cda7b71a782f4d107497bf861db0f183
Admin <Admin@DESKTOP-0BDN87K>
2022-08-28 15:19:03 -0300

    j

--- Loja_e-commerce/src/main/java/com/Loja_ecommerce/model/Pessoa.java
+++ Loja_e-commerce/src/main/java/com/Loja_ecommerce/model/Pessoa.java
@@ -1,5 +1,96 @@
 package com.Loja_ecommerce.model;
 
-public class Pessoa {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // para não criar no banco de dados uma pessoa abstração
+@SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", initialValue = 1, allocationSize = 1)
+public abstract class Pessoa { // abstração pra não ser invocada diretamente só os atributos
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa")
+	private Long id;
+
+	private String nome;
+
+	private String email;
+
+	private String telefone;
+
+	@OneToMany(mappedBy = "pessoa",orphanRemoval = true,cascade = CascadeType.ALL,fetch = FetchType.LAZY) /* Um para muitos, mappedby apontando para pessoa ,orpha usado quando remover uma pessoa do banco
+	 o endereço dela tbm é removido fetch.lazy para carregar endereço quando submitido */
+	private List<Endereco> enderecos = new ArrayList<Endereco>();
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
