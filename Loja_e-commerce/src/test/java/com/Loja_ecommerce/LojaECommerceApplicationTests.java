846e0b3de81e91aad78a2d969baee1a755caa4f3
Admin <Admin@DESKTOP-0BDN87K>
2022-08-28 11:55:33 -0300

    up

---
+++ Loja_e-commerce/src/test/java/com/Loja_ecommerce/LojaECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Loja_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
