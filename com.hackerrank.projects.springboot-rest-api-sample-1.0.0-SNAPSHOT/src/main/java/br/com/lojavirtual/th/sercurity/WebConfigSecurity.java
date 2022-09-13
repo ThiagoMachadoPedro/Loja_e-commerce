5dcd431492b8408d4fa985d0f729131e87394bca
Admin <Admin@DESKTOP-0BDN87K>
2022-09-13 20:08:04 -0300

    up

--- com.hackerrank.projects.springboot-rest-api-sample-1.0.0-SNAPSHOT/src/main/java/br/com/lojavirtual/th/sercurity/WebConfigSecurity.java
+++ com.hackerrank.projects.springboot-rest-api-sample-1.0.0-SNAPSHOT/src/main/java/br/com/lojavirtual/th/sercurity/WebConfigSecurity.java
@@ -7,10 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.com.lojavirtual.th.services.ImplementaUserDetailServices;
 
@@ -21,32 +25,48 @@ import br.com.lojavirtual.th.services.ImplementaUserDetailServices;
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
 
 	@Autowired
-	private ImplementaUserDetailServices implementacaoUserDetailServices;
-
+	private ImplementaUserDetailServices implementacaoUserDetailsService;
 	
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		.disable().authorizeRequests().antMatchers("/").permitAll()
+		.antMatchers("/index").permitAll()
+		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+		
+		/* redireciona ou da um retorno para index quando desloga*/
+		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+		
+		/*mapeia o logout do sistema*/
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		
+		/*Filtra as requisicoes para login de JWT*/
+		.and().addFilterAfter(new JWTLoginFilter("/login", authenticationManager()),
+				UsernamePasswordAuthenticationFilter.class)
+		
+		.addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
+		
+	}
 	
-	// consultar usuario no spring security login e senha do repository
+	
+	/*Irá consultar o user no banco com Spring Security*/
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		auth.userDetailsService(implementacaoUserDetailServices).
-															passwordEncoder(new BCryptPasswordEncoder());
+		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 		
-	
 	}
+	
+	
 
-	
-	
-	
-	
+	/*Ignora alguas URL livre de autenticação*/
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-
-		/* libera os end point ingnora algumas ulr */
-		web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso").antMatchers(HttpMethod.POST, "/salvarAcesso",
-				"/deletaAcesso");
-
+		//web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso")
+		//.antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
+		/*Ingnorando URL no momento para nao autenticar*/
 	}
 
 }
